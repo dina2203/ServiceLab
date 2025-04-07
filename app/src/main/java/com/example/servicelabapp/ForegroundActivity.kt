8abc4f217ef977b0a6e949384775039df95125e3
Dina <dina2203@iitu.edu.kz>
2025-04-07 17:03:52 +0500

    ForegroundActivity

--- app/src/main/java/com/example/servicelabapp/ForegroundActivity.kt
+++ app/src/main/java/com/example/servicelabapp/ForegroundActivity.kt
@@ -1,4 +1,31 @@
 package com.example.servicelabapp
 
-class ForegroundActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class ForegroundActivity : AppCompatActivity() {
+
+    private lateinit var musicServiceIntent: Intent
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_foreground)
+
+        musicServiceIntent = Intent(this, MyMusicService::class.java)
+    }
+
+    fun startMusic(view: View) {
+        ContextCompat.startForegroundService(this, musicServiceIntent)
+    }
+
+    fun stopMusic(view: View) {
+        stopService(musicServiceIntent)
+    }
+
+    fun back(view: View) {
+        finish()
+    }
 }
